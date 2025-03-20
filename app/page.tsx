8b4daec212ed8bea037aad095bf11a671b8a297e
notrealn <notrealnobodey@gmail.com>
2025-03-19 21:07:19 -0400

    mobile support

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,10 @@ export default function Home() {
     <main>
       <div className="bg-gradient-to-r from-black to-red-700 text-white text-center flex justify-evenly p-4 object-contain flex-wrap  gap-y-4">
         <div className="flex flex-col justify-evenly text-xl gap-2">
-          <h1 className="text-4xl font-bold">我们的宗旨 Our mission:</h1>
+          <div className="flex flex-row flex-wrap justify-center gap-x-2">
+            <h1 className="text-4xl font-bold">我们的宗旨</h1>
+            <h1 className="text-4xl font-bold">Our mission:</h1>
+          </div>
           <p className="max-w-lg">
             我们是一群乳腺癌患者成立的乳腺癌关怀机构，希望我们能在爱里，彼此陪伴，爱来癌去，一路同行！
           </p>
@@ -31,39 +34,43 @@ export default function Home() {
           </div>
         </div>
         <Image
-          className="min-w-0 max-w-sm h-auto"
+          className="min-w-0 max-w-sm w-full h-auto"
           src="/home/hands-together.png"
           alt="hands-together"
           width={494}
           height={449}
         />
       </div>
-      <div className="flex flex-row justify-evenly max-w-6xl m-4 ml-auto mr-auto">
+      <LinkContainer>
         <CircleLink text="关怀疗愈" svgPath="/home/heart2.svg" href="todo" />
         <CircleLink text="区域小组" svgPath="/home/map.svg" href="todo" />
         <CircleLink text="饮食健康" svgPath="/home/fruits.svg" href="todo" />
         <CircleLink text="运动康复" svgPath="/home/dumbell.svg" href="todo" />
-      </div>
+      </LinkContainer>
       <div className="text-center underline text-xl">最新活动更新</div>
-      <div className="flex flex-row justify-evenly max-w-6xl m-4 ml-auto mr-auto">
+      <LinkContainer>
         <RectLink text="周四活动" svgPath="/home/network.svg" href="todo" />
         <RectLink text="区域活动" svgPath="/home/world.svg" href="todo" />
         <RectLink text="病友探访" svgPath="/home/health.svg" href="todo" />
         <RectLink text="特殊活动" svgPath="/home/people.svg" href="todo" />
-      </div>
+      </LinkContainer>
       <div className="bg-gradient-to-r from-black to-red-700 text-white text-center text-xl p-2">
         当你孤单的时候，请相信，我们也正在找你，希望给你一个温暖的拥抱......
       </div>
-      <div className="flex flex-row justify-around m-6">
-        <div>
-          二维码联系
-          <br />
-          我们的义工
-        </div>
+      <div className="flex flex-row justify-around gap-x-2 m-6 flex-wrap">
+        <div>二维码联系我们的义工</div>
         <div>邮件发送给我们的义工</div>
         <div>每周四的时间，我们在Zoom等你来</div>
       </div>
     </main>
+  )
+}
+
+function LinkContainer({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex flex-row justify-evenly w-screen sm:w-[95vw] max-w-5xl overflow-x-auto m-4 ml-auto mr-auto">
+      {children}
+    </div>
   )
 }
 
@@ -77,11 +84,14 @@ function CircleLink({
   href: string
 }): ReactNode {
   return (
-    <a className="bg-amber-100 rounded-full p-16 relative m-4 mb-0" href={href}>
+    <a
+      className="bg-amber-100 rounded-xl p-10 relative text-xs md:p-14 md:text-base"
+      href={href}
+    >
       <div className="absolute flex flex-col top-0 bottom-0 left-0 right-0 m-auto w-fit h-fit items-center">
         {text}
         <Image
-          className="size-auto"
+          className="w-8 md:w-16"
           src={svgPath}
           alt="hearts"
           width={50}
@@ -103,13 +113,13 @@ function RectLink({
 }): ReactNode {
   return (
     <a
-      className="border-4 border-[#FDD1A3] rounded-xl p-16 relative m-4 box-border"
+      className="border-4 border-[#FDD1A3] rounded-xl p-9 relative box-border text-xs md:text-base md:p-14"
       href={href}
     >
       <div className="absolute flex flex-col top-0 bottom-0 left-0 right-0 m-auto w-fit h-fit items-center">
         {text}
         <Image
-          className="size-auto"
+          className="w-8 md:w-16"
           src={svgPath}
           alt="hearts"
           width={50}
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,7 +6,7 @@ import { usePathname } from 'next/navigation'
 export function NavBar() {
   return (
     <nav className="bg-gradient-to-r from-black to-red-700 pl-4 pr-4">
-      <div className="flex flex-row justify-between items-center flex-wrap p-2">
+      <div className="flex flex-row justify-between items-center flex-wrap p-2 gap-3">
         <div className="flex flex-row flex-initial">
           <Image
             className=""
@@ -20,24 +20,40 @@ export function NavBar() {
             <div>GIG FOREVER</div>
           </div>
         </div>
-        <div className="flex flex-row gap-3 p-3 grow text-white text-center max-md:p-0 max-md:w-screen max-md:justify-center max-md:order-last">
+        <label className="peer lg:hidden size-8">
+          <input className="peer hidden" type="checkbox" />
+          {/* <div className="bg-black size-full"></div> */}
+          <svg
+            className="peer-checked:hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+          >
+            {/*<!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->*/}
+            <path
+              fill="#fff"
+              d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"
+            />
+          </svg>
+          <svg
+            className="hidden peer-checked:block"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 384 512"
+          >
+            {/* <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--> */}
+            <path
+              fill="#fff"
+              d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
+            />
+          </svg>
+        </label>
+
+        <div className="hidden lg:flex peer-has-[:checked]:flex flex-row grow items-center peer-has-[:checked]:flex-col peer-has-[:checked]:items-start peer-has-[:checked]:w-full gap-3">
           <PageLink text="首页" href="/" />
           <PageLink text="区域活动" href="/todo" />
           <PageLink text="病友故事" href="/todo" />
           <PageLink text="社区资源" href="/todo" />
           <PageLink text="我们是谁" href="/about" />
-        </div>
-        <div className="">
-          <div className="flex flex-row items-center rounded-full bg-pink-200 shadow-lg">
-            <div className="text-xl p-3 pr-0">支持我们</div>
-            <Image
-              className="border-4 border-pink-200 bg-pink-200 rounded-full p-1 overflow-visible m-1 shadow-xl"
-              src="/support-us.svg"
-              alt="support us!"
-              width={48}
-              height={48}
-            />
-          </div>
+          <SupportUs />
         </div>
       </div>
     </nav>
@@ -47,10 +63,25 @@ export function NavBar() {
 function PageLink({ text, href }: { text: string; href: string }) {
   return (
     <a
-      className={`border-2 rounded-lg w-full max-w-24 border-white ${usePathname() === href ? 'bg-white/20' : ''}`}
+      className={`border-2 rounded-lg w-full max-w-24 text-center text-white border-white ${usePathname() === href ? 'bg-white/20' : ''}`}
       href={href}
     >
       {text}
     </a>
+  )
+}
+
+function SupportUs() {
+  return (
+    <div className="flex flex-row items-center rounded-full bg-pink-200 shadow-lg lg:ml-auto">
+      <div className="text-xl p-3 pr-0">支持我们</div>
+      <Image
+        className="border-4 border-pink-200 bg-pink-200 rounded-full p-1 overflow-visible m-1 shadow-xl"
+        src="/support-us.svg"
+        alt="support us!"
+        width={48}
+        height={48}
+      />
+    </div>
   )
 }
